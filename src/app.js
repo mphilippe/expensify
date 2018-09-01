6def108efdb4e4771cead7d137c0ac5deb550dcd
Mathias Philippe <ouebemaster@gmail.com>
2018-09-01 15:59:29 +0800

    Removed dummy expenses data

--- src/app.js
+++ src/app.js
@@ -11,20 +11,4 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-  description: 'Rent',
-  amount: 1260000,
-  createdAt: 1000
-}));
-store.dispatch(addExpense({
-  description: 'Grocery',
-  amount: 300000,
-  createdAt: -1000
-}));
-store.dispatch(addExpense({
-  description: 'Coffee',
-  amount: 10000,
-  createdAt: 10000
-}));
-
 ReactDOM.render(<AppRouter store={store}/>, document.getElementById('app'));
